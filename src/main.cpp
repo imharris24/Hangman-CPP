73ec009430c142a70ff7d87783e03297a3520b38
Muhammad Harris <harris20014@gmail.com>
2022-08-03 18:36:03 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,91 @@
 using namespace std;
 
 class Hangman {
-    private:
-
+    public:
+    bool GameOver;
+    int Lifes;
+    void DisplayHangman();
+    
 };
 
+void Hangman::DisplayHangman() {
+    if (Lifes==0) {
+       cout << "\n\t   +---+";
+       cout << "\n\t   |   |";
+       cout << "\n\t       |";
+       cout << "\n\t       |";
+       cout << "\n\t       |";
+       cout << "\n\t       |";
+       cout << "\n\t========";
+    }
+    else if(Lifes==1) {
+       cout << "\n\t   +---+";
+       cout << "\n\t   |   |";
+       cout << "\n\t   o   |";
+       cout << "\n\t       |";
+       cout << "\n\t       |";
+       cout << "\n\t       |";
+       cout << "\n\t========";
+    }
+    else if(Lifes==2) {
+       cout << "\n\t   +---+";
+       cout << "\n\t   |   |";
+       cout << "\n\t   o   |";
+       cout << "\n\t   |   |";
+       cout << "\n\t       |";
+       cout << "\n\t       |";
+       cout << "\n\t========";
+    }
+    else if(Lifes==3) {
+       cout << "\n\t   +---+";
+       cout << "\n\t   |   |";
+       cout << "\n\t   o   |";
+       cout << "\n\t  /|   |";
+       cout << "\n\t       |";
+       cout << "\n\t       |";
+       cout << "\n\t========";
+    }
+    else if(Lifes==4) {
+       cout << "\n\t   +---+";
+       cout << "\n\t   |   |";
+       cout << "\n\t   o   |";
+       cout << "\n\t  /|\\  |";
+       cout << "\n\t       |";
+       cout << "\n\t       |";
+       cout << "\n\t========";
+    }
+    else if(Lifes==5) {
+       cout << "\n\t   +---+";
+       cout << "\n\t   |   |";
+       cout << "\n\t   o   |";
+       cout << "\n\t  /|\\  |";
+       cout << "\n\t  /    |";
+       cout << "\n\t       |";
+       cout << "\n\t========";
+    }
+    else if(Lifes==7) {
+       cout << "\n\t   +---+";
+       cout << "\n\t   |   |";
+       cout << "\n\t   o   |";
+          cout << "\n\t  /|\\  |";
+       cout << "\n\t  / \\  |";
+       cout << "\n\t       |";
+       cout << "\n\t========";
+    }
+    cout << "\n";
+    }
+Hangman::Hangman() {
+    GameOver = false;
+    Lifes = -1;
+}
+
+
+
+
+
+
 int main() {
-    cout << "Hello, World!";
+    Hangman Game;
+
     return 0;
 }
